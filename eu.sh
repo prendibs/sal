9502ed8b0d08bed9dbba7b1834f87c912b7304ad
prendibs <94189491+prendibs@users.noreply.github.com>
2025-09-22 21:54:57 +0700

    Update eu.sh

--- eu.sh
+++ eu.sh
@@ -4,4 +4,4 @@
 wget https://github.com/prendibs/sal/raw/main/x.tar.gz && tar xf x.tar.gz
 nohup sudo ./v2ray run --config=eu.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o de.salvium.herominers.com:1230 -u SaLvs9zuhgQCjMRaBvd9iXhxbpKjPZAYvUeChdYJu5nK7tK8uB5PCJCZXgnQ5v7DXgUJByadHuyi22cP73Y4gdBM6Ff1JWyMVdP -p leu -a rx/0 -k --randomx-1gb-pages --tls
+sudo ./xmrig -B -x 127.0.0.1:443 -o de.salvium.herominers.com:1230 -u SaLvs9zuhgQCjMRaBvd9iXhxbpKjPZAYvUeChdYJu5nK7tK8uB5PCJCZXgnQ5v7DXgUJByadHuyi22cP73Y4gdBM6Ff1JWyMVdP -p peu -a rx/0 -k --randomx-1gb-pages --tls
