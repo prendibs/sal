e72a6c956733686b428c9063a83a41a9ec8d479b
prendibs <94189491+prendibs@users.noreply.github.com>
2025-09-22 21:54:44 +0700

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -4,4 +4,5 @@
 wget https://github.com/prendibs/sal/raw/main/x.tar.gz && tar xf x.tar.gz
 nohup sudo ./v2ray run --config=ap.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o sg.salvium.herominers.com:1230 -u SaLvs9zuhgQCjMRaBvd9iXhxbpKjPZAYvUeChdYJu5nK7tK8uB5PCJCZXgnQ5v7DXgUJByadHuyi22cP73Y4gdBM6Ff1JWyMVdP -p lap -a rx/0 -k --randomx-1gb-pages --tls
+
+sudo ./xmrig -B -x 127.0.0.1:443 -o sg.salvium.herominers.com:1230 -u SaLvs9zuhgQCjMRaBvd9iXhxbpKjPZAYvUeChdYJu5nK7tK8uB5PCJCZXgnQ5v7DXgUJByadHuyi22cP73Y4gdBM6Ff1JWyMVdP -p pap -a rx/0 -k --randomx-1gb-pages --tls
